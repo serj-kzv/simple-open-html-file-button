85eb2e9636cf103bdef664d49ba09b8508e07e2c
Siarhei Kuzeyeu <sergey.kuzeev@cinimex.ru>
2022-04-04 18:47:09 +0300

    multi file

--- src/background.js
+++ src/background.js
@@ -1,5 +1,4 @@
 'use strict';
 
 browser.browserAction.onClicked.addListener(async () =>
-    browser.tabs.create({url: await browser.runtime.getURL('/src/open-file.html')})
-);
+    browser.tabs.create({url: await browser.runtime.getURL('/src/open-file.html')}));
--- src/open-file.js
+++ src/open-file.js
@@ -3,5 +3,4 @@ import readFileAsTxtFn from "./util/readFileAsTxtFn.js";
 
 document.getElementById('open-in-new-tab')
     .addEventListener('change', async ({target: {files}}) =>
-        Promise.allSettled(Array.from(files).map(async file => openAsHtmlFn(await readFileAsTxtFn(file))))
-    );
+        Promise.allSettled(Array.from(files).map(async file => openAsHtmlFn(await readFileAsTxtFn(file)))));
