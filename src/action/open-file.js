dd9e0ac147117e2076e2620b6bd80c557eb2f906
null <null>
2022-04-11 23:30:22 +0300

    v13

--- src/action/open-file.js
+++ src/action/open-file.js
@@ -5,38 +5,26 @@ import openAsHtmlFn from "../util/openAsHtmlFn.js";
 import readFileAsTxtFn from "../util/readFileAsTxtFn.js";
 
 document.getElementById(CONSTANTS.openInNewTab).addEventListener('change', async ({target: {files}}) => {
-    files = Array.from(files);
-
     const config = await getOrDefaultFn();
     const readHtmlAsTxtFn = Boolean(config[CONSTANTS.detectEncodingEnabled]) ? fileToTxtWithEncodingDetectionFn : readFileAsTxtFn;
+    const openAsHtmlFnPromises = Array.from(files).map(async file => await openAsHtmlFn(
+        await readHtmlAsTxtFn(file, config[CONSTANTS.quantityOfBytesToDetectEncoding]),
+        'UTF-8',
+        false,
+        true,
+        Boolean(config[CONSTANTS.clearMemoryOnRemoved]),
+        Boolean(config[CONSTANTS.clearMemoryOnReplaced]),
+        Boolean(config[CONSTANTS.clearMemoryOnUpdated])
+    ));
 
     if (config[CONSTANTS.sequentialOpening]) {
-        for (const file of files) {
+        for (const openAsHtmlFnPromise of openAsHtmlFnPromises) {
             try {
-                await openAsHtmlFn(
-                    await readHtmlAsTxtFn(file, config[CONSTANTS.quantityOfBytesToDetectEncoding]),
-                    'UTF-8',
-                    false,
-                    true,
-                    Boolean(config[CONSTANTS.clearMemoryOnRemoved]),
-                    Boolean(config[CONSTANTS.clearMemoryOnReplaced]),
-                    Boolean(config[CONSTANTS.clearMemoryOnUpdated])
-                );
+                await openAsHtmlFnPromise;
             } catch (e) {
-
             }
         }
     } else {
-        Promise.allSettled(
-            files.map(async file => openAsHtmlFn(
-                await readHtmlAsTxtFn(file, config[CONSTANTS.quantityOfBytesToDetectEncoding]),
-                'UTF-8',
-                false,
-                true,
-                Boolean(config[CONSTANTS.clearMemoryOnRemoved]),
-                Boolean(config[CONSTANTS.clearMemoryOnReplaced]),
-                Boolean(config[CONSTANTS.clearMemoryOnUpdated])
-            ))
-        );
+        Promise.allSettled(openAsHtmlFnPromises);
     }
 });
